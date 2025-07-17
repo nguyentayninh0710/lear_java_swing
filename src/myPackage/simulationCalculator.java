f3e2cdc108da2539c7706b2fa42bb14977ba42a1
nguyentayninh0710 <huynhkykhoinguyen0710@gmail.com>
2025-07-17 21:21:24 +0700

    add button 1

--- src/myPackage/simulationCalculator.java
+++ src/myPackage/simulationCalculator.java
@@ -34,6 +34,7 @@ public class simulationCalculator extends JFrame {
 	private JButton btnnum4;
 	private JButton btnnum1;
 	private JButton btnnum0;
+	private String calculatorString;
 
 	/**
 	 * Launch the application.
@@ -61,7 +62,7 @@ public class simulationCalculator extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		textNumber = new JTextField();
 		textNumber.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		textNumber.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -69,67 +70,67 @@ public class simulationCalculator extends JFrame {
 		textNumber.setBounds(11, 6, 285, 65);
 		contentPane.add(textNumber);
 		textNumber.setColumns(10);
-		
+
 		JButton btnDivide = new JButton("/");
 		btnDivide.setBounds(226, 70, 72, 37);
 		contentPane.add(btnDivide);
-		
+
 		JButton btnMultiply = new JButton("x");
 		btnMultiply.setBounds(226, 110, 72, 37);
 		contentPane.add(btnMultiply);
-		
+
 		btnMinus = new JButton("-");
 		btnMinus.setBounds(226, 150, 72, 37);
 		contentPane.add(btnMinus);
-		
+
 		btnSum = new JButton("+");
 		btnSum.setBounds(226, 190, 72, 37);
 		contentPane.add(btnSum);
-		
+
 		btnResult = new JButton("=");
 		btnResult.setBounds(226, 230, 72, 37);
 		contentPane.add(btnResult);
-		
+
 		btnPercent = new JButton("%");
 		btnPercent.setBounds(153, 70, 72, 37);
 		contentPane.add(btnPercent);
-		
+
 		btnnum9 = new JButton("9");
 		btnnum9.setBounds(153, 110, 72, 37);
 		contentPane.add(btnnum9);
-		
+
 		btnnum6 = new JButton("6");
 		btnnum6.setBounds(153, 150, 72, 37);
 		contentPane.add(btnnum6);
-		
+
 		btnnum3 = new JButton("3");
 		btnnum3.setBounds(153, 190, 72, 37);
 		contentPane.add(btnnum3);
-		
+
 		btnComma = new JButton(",");
 		btnComma.setBounds(153, 229, 72, 37);
 		contentPane.add(btnComma);
-		
+
 		btnChangeSign = new JButton("+/-");
 		btnChangeSign.setBounds(81, 70, 72, 37);
 		contentPane.add(btnChangeSign);
-		
+
 		btnnum8 = new JButton("8");
 		btnnum8.setBounds(81, 110, 72, 37);
 		contentPane.add(btnnum8);
-		
+
 		btnnum5 = new JButton("5");
 		btnnum5.setBounds(81, 150, 72, 37);
 		contentPane.add(btnnum5);
-		
+
 		btnnum2 = new JButton("2");
 		btnnum2.setBounds(81, 190, 72, 37);
 		contentPane.add(btnnum2);
-		
+
 		btnAC = new JButton("AC");
 		btnAC.setBounds(9, 70, 72, 37);
 		contentPane.add(btnAC);
-		
+
 		btnnum7 = new JButton("7");
 		btnnum7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -137,15 +138,26 @@ public class simulationCalculator extends JFrame {
 		});
 		btnnum7.setBounds(9, 110, 72, 37);
 		contentPane.add(btnnum7);
-		
+
 		btnnum4 = new JButton("4");
 		btnnum4.setBounds(9, 150, 72, 37);
 		contentPane.add(btnnum4);
-		
+
 		btnnum1 = new JButton("1");
+		btnnum1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				calculatorString = textNumber.getText();
+				if (calculatorString.equals("0")) {
+					calculatorString = "1";
+				} else {
+					calculatorString += "1";
+				}
+				textNumber.setText(calculatorString);
+			}
+		});
 		btnnum1.setBounds(9, 190, 72, 37);
 		contentPane.add(btnnum1);
-		
+
 		btnnum0 = new JButton("0");
 		btnnum0.setBounds(9, 230, 144, 37);
 		contentPane.add(btnnum0);
